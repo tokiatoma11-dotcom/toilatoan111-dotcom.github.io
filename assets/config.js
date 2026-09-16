db4bf4332caffff94c1c0d10f8b07eb31361d297
tokiatoma11-dotcom <tokiatoma11@gmail.com>
2026-09-16 13:06:07 +0700

    Move API keys and config to config.js
    Refactor configuration and API key management into a separate config file.

---
+++ assets/config.js
@@ -0,0 +1,27 @@
+// assets/config.js
+const CONFIG = {
+    // Danh sách 11 API Key Gemini
+    GEMINI_API_KEYS: [
+        "AQ.Ab8RN6ITpaGq7IreWiKjDXRMs_Wc6cE9do_JzpSDG2-45WC79Q",
+        "AQ.Ab8RN6KEabKP85u7808fHQ5jQZu_iIF01RjAoFqxaWAQFI86ZA",
+        "AQ.Ab8RN6JbX8nQohDj9vxd3vMOWTAQ98OD_3Nl6cNjA9g5c-DmZg",
+        "AQ.Ab8RN6LH20fDJdDCUMX0cEee5Q8GQfxDb8NFSWFzrVS8Yk9XUQ",
+        "AQ.Ab8RN6K8_R6h_z1lI6a_h2wU-yIAJLwB1dWOpIq7KdBIuXObHg",
+        "AQ.Ab8RN6Ktn4JgxS4OkioPQKf6nb4BrLMs4za4FJj6_SbiyXNAHA",
+        "AQ.Ab8RN6KcR_QRGOLB5EEuCyPBuvB09Z0N7cBA2ctECkuLcJ25eQ",
+        "AQ.Ab8RN6IveejKg0xbR48bxhmrBApLbPs0dAATWi7mUNHz9XUoOQ",
+        "AQ.Ab8RN6KflkQ03zo10naDLvqH5mmSsTgBkUNm53lMxedx-TA3iA",
+        "AQ.Ab8RN6JDFM65tm4EDcCHQpc8U20vF7YfLXrBq2z4jFyGWvqG2A",
+        "AQ.Ab8RN6K7Y4Hk5RzcMuDtHY0PshVBXZmCGEs14aDxIJNw_YxjXw"
+    ],
+    // API Key Groq
+    GROQ_API_KEY: "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",
+    
+    // Cấu hình mặc định
+    DEFAULT_MODEL: "gpt_oss_120b",
+    DEFAULT_EFFORT: "medium",
+    IS_THINKING_ENABLED: true
+};
+
+export default CONFIG;
+
